636fe84b21e84b1a112aa6eb0d767a6c0677139e
Falko Strenzke <falko.strenzke@mtg.de>
2023-08-22 11:32:10 +0200

    some further adaption to gnu coding rules

--- cipher/kyber_symmetric.h
+++ cipher/kyber_symmetric.h
@@ -12,6 +12,18 @@
 
 void _gcry_kyber_shake128_absorb(gcry_md_hd_t h, const unsigned char seed[GCRY_KYBER_SYMBYTES], unsigned char x, unsigned char y);
 
+/*************************************************
+ * Name:        kyber_shake256_prf
+ *
+ * Description: Usage of SHAKE256 as a PRF, concatenates secret and public
+ *input and then generates outlen bytes of SHAKE256 output
+ *
+ * Arguments:   - unsigned char *out: pointer to output
+ *              - size_t outlen: number of requested output bytes
+ *              - const unsigned char *key: pointer to the key (of length
+ *GCRY_KYBER_SYMBYTES)
+ *              - unsigned char nonce: single-byte nonce (public PRF input)
+ **************************************************/
 gcry_err_code_t _gcry_kyber_shake256_prf(uint8_t *out, size_t outlen, const uint8_t key[GCRY_KYBER_SYMBYTES], uint8_t nonce);
 
 gcry_err_code_t _gcry_kyber_shake128_squeezeblocks(gcry_md_hd_t h, uint8_t *out, size_t nblocks );
