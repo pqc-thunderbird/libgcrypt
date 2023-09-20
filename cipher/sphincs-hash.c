666cfc834ac72e80846825f491f0a3a34919c4a9
Johannes Roth <johannes.roth@mtg.de>
2023-09-20 11:44:22 +0200

    replace shax_ctx

--- cipher/sphincs-hash.c
+++ cipher/sphincs-hash.c
@@ -95,45 +95,13 @@ void hash_message(unsigned char *digest, uint64_t *tree, uint32_t *leaf_idx,
         hash_message_shake(digest, tree, leaf_idx, R, pk, m, mlen, ctx);
     }
 }
-typedef struct {
-    uint8_t shax_output_bytes;
-    uint8_t shax_block_bytes;
-    void (*shaX_inc_init)(uint8_t*);
-    void (*shaX_inc_blocks)(uint8_t*, const uint8_t*, size_t);
-    void (*shaX_inc_finalize)(uint8_t*, uint8_t*, const uint8_t*, size_t);
-    void (*shaX)(uint8_t*, const uint8_t*, size_t);
-    void (*mgf1_X)(unsigned char*, unsigned long, const unsigned char*, unsigned long);
-} shaX_cfg;
-
-static void shaX_cfg_init(shaX_cfg *ctx, uint8_t n)
-{
-    if(n >= 24)
-    {
-        ctx->shax_output_bytes = SPX_SHA512_OUTPUT_BYTES;
-        ctx->shax_block_bytes = SPX_SHA512_BLOCK_BYTES;
-        ctx->shaX_inc_init = &sha512_inc_init;
-        ctx->shaX_inc_blocks = &sha512_inc_blocks;
-        ctx->shaX_inc_finalize = &sha512_inc_finalize;
-        ctx->shaX = &sha512;
-        ctx->mgf1_X = &mgf1_512;
-    }
-    else {
-        ctx->shax_output_bytes = SPX_SHA256_OUTPUT_BYTES;
-        ctx->shax_block_bytes = SPX_SHA256_BLOCK_BYTES;
-        ctx->shaX_inc_init = &sha256_inc_init;
-        ctx->shaX_inc_blocks = &sha256_inc_blocks;
-        ctx->shaX_inc_finalize = &sha256_inc_finalize;
-        ctx->shaX = &sha256;
-        ctx->mgf1_X = &mgf1_256;
-    }
-}
-
 
 /* For SHA, there is no immediate reason to initialize at the start,
    so this function is an empty operation. */
 void initialize_hash_function_sha2(spx_ctx *ctx)
 {
-    seed_state(ctx);
+    /* TODO: implement this speed optimization */
+    //seed_state(ctx);
 }
 
 /*
@@ -142,18 +110,6 @@ void initialize_hash_function_sha2(spx_ctx *ctx)
 static void prf_addr_sha2(unsigned char *out, const spx_ctx *ctx,
               const uint32_t addr[8])
 {
-    //uint8_t sha2_state[40];
-    //unsigned char buf[SPX_SHA256_ADDR_BYTES + ctx->n];
-    //unsigned char outbuf[SPX_SHA256_OUTPUT_BYTES];
-
-    ///* Retrieve precomputed state containing pub_seed */
-    //memcpy(sha2_state, ctx->state_seeded, 40 * sizeof(uint8_t));
-
-    ///* Remainder: ADDR^c ‖ SK.seed */
-    //memcpy(buf, addr, SPX_SHA256_ADDR_BYTES);
-    //memcpy(buf + SPX_SHA256_ADDR_BYTES, ctx->sk_seed, ctx->n);
-
-    //sha256_inc_finalize(outbuf, sha2_state, buf, SPX_SHA256_ADDR_BYTES + ctx->n);
     unsigned char sha256_pubseed_block[SPX_SHA256_BLOCK_BYTES];
     memset(sha256_pubseed_block, 0, SPX_SHA256_BLOCK_BYTES);
     memcpy(sha256_pubseed_block, ctx->pub_seed, ctx->n);
@@ -163,7 +119,6 @@ static void prf_addr_sha2(unsigned char *out, const spx_ctx *ctx,
     _gcry_md_write(hd, sha256_pubseed_block, SPX_SHA256_BLOCK_BYTES);
     _gcry_md_write(hd, (uint8_t*)addr, SPX_SHA256_ADDR_BYTES);
     _gcry_md_write(hd, ctx->sk_seed, ctx->n);
-    //_gcry_md_write(hd, buf, SPX_SHA256_ADDR_BYTES + ctx->n);
     memcpy(out, _gcry_md_read(hd, GCRY_MD_SHA256), ctx->n);
     _gcry_md_close(hd);
 
@@ -172,7 +127,7 @@ static void prf_addr_sha2(unsigned char *out, const spx_ctx *ctx,
 /**
  * Computes the message-dependent randomness R, using a secret seed as a key
  * for HMAC, and an optional randomization value prefixed to the message.
- * This requires m to have at least shax_ctx.shax_block_bytes + ctx->n space
+ * This requires m to have at least shax_block_bytes + ctx->n space
  * available in front of the pointer, i.e. before the message to use for the
  * prefix. This is necessary to prevent having to move the message around (and
  * allocate memory for it).
@@ -182,7 +137,7 @@ static gcry_err_code_t gen_message_random_sha2(unsigned char *R, const unsigned 
                         const unsigned char *m, unsigned long long mlen,
                         const spx_ctx *ctx)
 {
-    // HMAC-SHA-X(SK.prf, OptRand||M)
+    /* HMAC-SHA-X(SK.prf, OptRand||M) */
 
     int hmac_shaX_algo;
     gcry_mac_hd_t hd = NULL;
@@ -248,82 +203,76 @@ static void hash_message_sha2(unsigned char *digest, uint64_t *tree, uint32_t *l
                   const unsigned char *m, unsigned long long mlen,
                   const spx_ctx *ctx)
 {
-    (void)ctx;
-    shaX_cfg shax_ctx;
-    shaX_cfg_init(&shax_ctx, ctx->n);
-//#define SPX_TREE_BITS (ctx->tree_height * (ctx->d - 1))
-//#define SPX_TREE_BYTES ((SPX_TREE_BITS + 7) / 8)
-//#define SPX_LEAF_BITS ctx->tree_height
-//#define SPX_LEAF_BYTES ((SPX_LEAF_BITS + 7) / 8)
-//#define SPX_DGST_BYTES (ctx->FORS_msg_bytes + SPX_TREE_BYTES + SPX_LEAF_BYTES)
     size_t SPX_TREE_BITS = (ctx->tree_height * (ctx->d - 1));
     size_t SPX_TREE_BYTES = ((SPX_TREE_BITS + 7) / 8);
     size_t SPX_LEAF_BITS = ctx->tree_height;
     size_t SPX_LEAF_BYTES = ((SPX_LEAF_BITS + 7) / 8);
     size_t SPX_DGST_BYTES = (ctx->FORS_msg_bytes + SPX_TREE_BYTES + SPX_LEAF_BYTES);
+    int hash_alg;
+    uint8_t shax_block_bytes;
+    uint8_t shax_output_bytes;
 
-    unsigned char seed[2*ctx->n + shax_ctx.shax_output_bytes];
+    if(ctx->do_use_sha512)
+    {
+        hash_alg = GCRY_MD_SHA512;
+        shax_block_bytes = SPX_SHA512_BLOCK_BYTES;
+        shax_output_bytes = SPX_SHA512_OUTPUT_BYTES;
+    }
+    else {
+        hash_alg = GCRY_MD_SHA256;
+        shax_block_bytes = SPX_SHA256_BLOCK_BYTES;
+        shax_output_bytes = SPX_SHA256_OUTPUT_BYTES;
+    }
 
-    /* Round to nearest multiple of shax_ctx.shax_block_bytes */
-// TODO: assert?
-//#if (shax_ctx.shax_block_bytes & (shax_ctx.shax_block_bytes-1)) != 0
-//    #error "Assumes that shax_ctx.shax_block_bytes is a power of 2"
-//#endif
-#define SPX_INBLOCKS (((ctx->n + ctx->public_key_bytes + shax_ctx.shax_block_bytes - 1) & \
-                        -shax_ctx.shax_block_bytes) / shax_ctx.shax_block_bytes)
-    unsigned char inbuf[SPX_INBLOCKS * shax_ctx.shax_block_bytes];
+    unsigned char seed[2*ctx->n + shax_output_bytes];
+
+    size_t SPX_INBLOCKS = (((ctx->n + ctx->public_key_bytes + shax_block_bytes - 1) &
+                        -shax_block_bytes) / shax_block_bytes);
+
+    unsigned char inbuf[SPX_INBLOCKS * shax_block_bytes];
 
     unsigned char buf[SPX_DGST_BYTES];
     unsigned char *bufp = buf;
-    uint8_t state[8 + shax_ctx.shax_output_bytes];
 
-    shax_ctx.shaX_inc_init(state);
 
     // seed: SHA-X(R ‖ PK.seed ‖ PK.root ‖ M)
     memcpy(inbuf, R, ctx->n);
     memcpy(inbuf + ctx->n, pk, ctx->public_key_bytes);
 
+     gcry_md_hd_t hd;
+    /* TODO: md_open can give error code... */
+    _gcry_md_open (&hd, hash_alg, GCRY_MD_FLAG_SECURE);
+
     /* If R + pk + message cannot fill up an entire block */
-    if (ctx->n + ctx->public_key_bytes + mlen < SPX_INBLOCKS * shax_ctx.shax_block_bytes) {
+    if (ctx->n + ctx->public_key_bytes + mlen < SPX_INBLOCKS * shax_block_bytes) {
         memcpy(inbuf + ctx->n + ctx->public_key_bytes, m, mlen);
-        shax_ctx.shaX_inc_finalize(seed + 2*ctx->n, state, inbuf, ctx->n + ctx->public_key_bytes + mlen);
+        _gcry_md_write(hd, inbuf, SPX_INBLOCKS*shax_block_bytes);
     }
     /* Otherwise first fill a block, so that finalize only uses the message */
     else {
         memcpy(inbuf + ctx->n + ctx->public_key_bytes, m,
-               SPX_INBLOCKS * shax_ctx.shax_block_bytes - ctx->n - ctx->public_key_bytes);
-        shax_ctx.shaX_inc_blocks(state, inbuf, SPX_INBLOCKS);
+               SPX_INBLOCKS * shax_block_bytes - ctx->n - ctx->public_key_bytes);
 
-        m += SPX_INBLOCKS * shax_ctx.shax_block_bytes - ctx->n - ctx->public_key_bytes;
-        mlen -= SPX_INBLOCKS * shax_ctx.shax_block_bytes - ctx->n - ctx->public_key_bytes;
-        shax_ctx.shaX_inc_finalize(seed + 2*ctx->n, state, m, mlen);
+        _gcry_md_write(hd, inbuf, SPX_INBLOCKS*shax_block_bytes);
+        m += SPX_INBLOCKS * shax_block_bytes - ctx->n - ctx->public_key_bytes;
+        mlen -= SPX_INBLOCKS * shax_block_bytes - ctx->n - ctx->public_key_bytes;
+        _gcry_md_write(hd, m, mlen);
+
     }
+    memcpy(seed + 2*ctx->n, _gcry_md_read(hd, hash_alg), shax_output_bytes);
+    _gcry_md_close(hd);
+
 
     // H_msg: MGF1-SHA-X(R ‖ PK.seed ‖ seed)
     memcpy(seed, R, ctx->n);
     memcpy(seed + ctx->n, pk, ctx->n);
 
-    /* By doing this in two steps, we prevent hashing the message twice;
-       otherwise each iteration in MGF1 would hash the message again. */
-    //shax_ctx.mgf1_X(bufp, SPX_DGST_BYTES, seed, 2*ctx->n + shax_ctx.shax_output_bytes);
-    int algo;
-    if(ctx->do_use_sha512)
-    {
-        algo = GCRY_MD_SHA512;
-    }
-    else {
-        algo = GCRY_MD_SHA256;
-    }
     // TODO check err
-    mgf1(bufp, SPX_DGST_BYTES, seed, 2*ctx->n + shax_ctx.shax_output_bytes, algo);
+    mgf1(bufp, SPX_DGST_BYTES, seed, 2*ctx->n + shax_output_bytes, hash_alg);
 
 
     memcpy(digest, bufp, ctx->FORS_msg_bytes);
     bufp += ctx->FORS_msg_bytes;
-
-#if SPX_TREE_BITS > 64
-    #error For given height and depth, 64 bits cannot represent all subtrees
-#endif
 
     *tree = bytes_to_ull(bufp, SPX_TREE_BYTES);
     *tree &= (~(uint64_t)0) >> (64 - SPX_TREE_BITS);
@@ -372,19 +321,6 @@ static void gen_message_random_shake(unsigned char *R, const unsigned char *sk_p
                         const unsigned char *m, unsigned long long mlen,
                         const spx_ctx *ctx)
 {
-    (void)ctx;
-
-/*
-    uint64_t s_inc[26];
-
-    shake256_inc_init(s_inc);
-    shake256_inc_absorb(s_inc, sk_prf, ctx->n);
-    shake256_inc_absorb(s_inc, optrand, ctx->n);
-    shake256_inc_absorb(s_inc, m, mlen);
-    shake256_inc_finalize(s_inc);
-    shake256_inc_squeeze(R, ctx->n, s_inc);
-*/
-
     gcry_md_hd_t hd;
 
     /* TODO: check and return err */
@@ -406,12 +342,11 @@ static void hash_message_shake(unsigned char *digest, uint64_t *tree, uint32_t *
                   const unsigned char *m, unsigned long long mlen,
                   const spx_ctx *ctx)
 {
-    (void)ctx;
-#define SPX_TREE_BITS (ctx->tree_height * (ctx->d - 1))
-#define SPX_TREE_BYTES ((SPX_TREE_BITS + 7) / 8)
-#define SPX_LEAF_BITS ctx->tree_height
-#define SPX_LEAF_BYTES ((SPX_LEAF_BITS + 7) / 8)
-#define SPX_DGST_BYTES (ctx->FORS_msg_bytes + SPX_TREE_BYTES + SPX_LEAF_BYTES)
+    size_t SPX_TREE_BITS = ctx->tree_height * (ctx->d - 1);
+    size_t SPX_TREE_BYTES = (SPX_TREE_BITS + 7) / 8;
+    size_t SPX_LEAF_BITS = ctx->tree_height;
+    size_t SPX_LEAF_BYTES = (SPX_LEAF_BITS + 7) / 8;
+    size_t SPX_DGST_BYTES = ctx->FORS_msg_bytes + SPX_TREE_BYTES + SPX_LEAF_BYTES;
 
     unsigned char buf[SPX_DGST_BYTES];
     unsigned char *bufp = buf;
@@ -426,11 +361,6 @@ static void hash_message_shake(unsigned char *digest, uint64_t *tree, uint32_t *
 
     memcpy(digest, bufp, ctx->FORS_msg_bytes);
     bufp += ctx->FORS_msg_bytes;
-
-/* TODO: assert? */
-// #if SPX_TREE_BITS > 64
-//     #error For given height and depth, 64 bits cannot represent all subtrees
-// #endif
 
     *tree = bytes_to_ull(bufp, SPX_TREE_BYTES);
     *tree &= (~(uint64_t)0) >> (64 - SPX_TREE_BITS);
