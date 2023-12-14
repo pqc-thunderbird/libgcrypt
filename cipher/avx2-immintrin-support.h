0d0dd94a0a99fdc9693f1581ee71190af442659a
Johannes Roth <johannes.roth@mtg.de>
2023-12-14 10:00:02 +0100

    add check for immintrin.h

---
+++ cipher/avx2-immintrin-support.h
@@ -0,0 +1,10 @@
+#include "config.h"
+
+/* USE_AVX2 indicates whether to compile with Intel AVX2 code. */
+#undef USE_AVX2
+#if defined(__x86_64__) && defined(HAVE_GCC_INLINE_ASM_AVX2) && \
+    defined(HAVE_IMMINTRIN_H) && \
+    (defined(HAVE_COMPATIBLE_GCC_AMD64_PLATFORM_AS) || \
+     defined(HAVE_COMPATIBLE_GCC_WIN64_PLATFORM_AS))
+# define USE_AVX2 1
+#endif
